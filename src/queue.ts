5b4e5e16be078bf0714abe9c968027e69c477579
Andrey Marchenko <andrey.marchenko@booking.com>
2023-09-22 21:49:03 +0100

    fix(queue): return previous algorithm. last implementation has a bug

--- src/queue.ts
+++ src/queue.ts
@@ -1,52 +1,72 @@
+interface ListNode<Value> {
+  next: ListNode<Value> | null;
+  prev: ListNode<Value> | null;
+  value: Value;
+}
+
 export class Queue<Value> {
-  private newStack: Value[] = [];
-  private oldStack: Value[] = [];
-  private oldStackFirstIndex = 0;
+  length = 0;
+  private start: ListNode<Value> | null = null;
+  private end: ListNode<Value> | null = null;
 
   push(value: Value): void {
-    this.newStack.push(value);
+    const newNode = {
+      value,
+      next: null,
+      prev: null,
+    };
+    this.length++;
+    if (this.start === null) {
+      this.start = newNode;
+      this.end = newNode;
+      return;
+    }
+
+    const currentEnd = this.end;
+    this.end = newNode;
+    this.end.prev = currentEnd;
+    if (currentEnd !== null) {
+      currentEnd.next = this.end;
+    }
   }
 
   pop() {
-    if (this.newStack.length !== 0) {
-      return this.newStack.pop();
-    } else if (this.oldStack.length !== 0) {
-      return this.oldStack.pop();
+    if (this.end === null) {
+      return null;
+    }
+    this.length--;
+    // if equal we have only 1 node, so we just remove start
+    if (this.end === this.start) {
+      this.start = null;
+    }
+    const { value } = this.end;
+    this.end = this.end.prev;
+    if (this.end) {
+      this.end.next = null;
     }
 
-    return null;
+    return value;
   }
 
   shift() {
-    if (this.size() === 0) {
+    if (this.start === null) {
       return null;
     }
-
-    if (this.oldStack.length === 0 && this.newStack.length !== 0) {
-      this.oldStack = this.newStack;
-      this.oldStackFirstIndex = 0;
-      this.newStack = [];
+    this.length--;
+    // if equal we have only 1 node, so we just remove end
+    if (this.end === this.start) {
+      this.end = null;
+    }
+    const { value } = this.start;
+    this.start = this.start.next;
+    if (this.start) {
+      this.start.prev = null;
     }
 
-    if (this.oldStack.length !== 0 && this.oldStackFirstIndex < this.oldStack.length) {
-      const value = this.oldStack[this.oldStackFirstIndex];
-
-      //@ts-ignore
-      this.oldStack[this.oldStackFirstIndex] = null;
-      this.oldStackFirstIndex++;
-
-      if (this.oldStackFirstIndex === this.oldStack.length) {
-        this.oldStack = [];
-        this.oldStackFirstIndex = 0;
-      }
-
-      return value;
-    }
-
-    return null;
+    return value;
   }
 
   size() {
-    return this.newStack.length + this.oldStack.length - this.oldStackFirstIndex;
+    return this.length;
   }
 }
